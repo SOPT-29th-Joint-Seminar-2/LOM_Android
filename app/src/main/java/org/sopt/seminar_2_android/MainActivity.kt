cc61f3a413775b278c8f17278d78c1395c04f307
hotaek <kht072623@naver.com>
2021-11-15 20:37:10 +0900

    CHORE

--- app/src/main/java/org/sopt/seminar_2_android/MainActivity.kt
+++ app/src/main/java/org/sopt/seminar_2_android/MainActivity.kt
@@ -2,10 +2,61 @@ package org.sopt.seminar_2_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import org.sopt.seminar_2_android.databinding.ActivityMainBinding
+import org.sopt.seminar_2_android.ui.FeedFragment
+import org.sopt.seminar_2_android.ui.base.BaseActivity
+import org.sopt.seminar_2_android.ui.home.HomeFragment
+import org.sopt.seminar_2_android.ui.mylibrary.MyLibraryFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private lateinit var mainViewPagerAdapter: MainViewPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initViewPager()
+        iniBottomNavigation()
+    }
+
+    private fun initViewPager(){
+        val fragmentList = listOf(HomeFragment(), FeedFragment(), MyLibraryFragment())
+
+        mainViewPagerAdapter = MainViewPagerAdapter(this)
+        mainViewPagerAdapter.fragments.addAll(fragmentList)
+        binding.vpMain.adapter = mainViewPagerAdapter
+    }
+
+
+    private fun iniBottomNavigation(){
+        with(binding){
+            vpMain.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    bnvMain.menu.getItem(position).isChecked = true
+                }
+            })
+
+            bnvMain.setOnItemSelectedListener {
+                when(it.itemId){
+                    R.id.item_home -> {
+                        vpMain.currentItem = HOME_FRAGMENT
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.item_feed -> {
+                        vpMain.currentItem = FEED_FRAGMENT
+                        return@setOnItemSelectedListener true
+                    }
+
+                    else -> {
+                        vpMain.currentItem = LIBRARY_FRAGMENT
+                        return@setOnItemSelectedListener true
+                    }
+                }
+            }
+        }
+    }
+
+    companion object{
+        const val HOME_FRAGMENT = 0
+        const val FEED_FRAGMENT = 1
+        const val LIBRARY_FRAGMENT = 2
     }
 }

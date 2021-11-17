0ab362266a692d64b0d84b433d9b6d2f0ec93de9
hotaek <kht072623@naver.com>
2021-11-17 16:11:28 +0900

    [UI] best_book_fragment

---
+++ app/src/main/java/org/sopt/seminar_2_android/ui/home/BestBookFragment.kt
@@ -0,0 +1,17 @@
+package org.sopt.seminar_2_android.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.sopt.seminar_2_android.R
+import org.sopt.seminar_2_android.databinding.FragmentBestBookBinding
+import org.sopt.seminar_2_android.ui.base.BaseFragment
+
+
+class BestBookFragment : BaseFragment<FragmentBestBookBinding>(R.layout.fragment_best_book) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

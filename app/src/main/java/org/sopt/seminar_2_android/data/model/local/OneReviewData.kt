316f6f701379d4b1bfe9c091d68c58490005f1d6
hotaek <kht072623@naver.com>
2021-11-19 02:49:41 +0900

    [FEAT] best_book_fragment_end

---
+++ app/src/main/java/org/sopt/seminar_2_android/data/model/local/OneReviewData.kt
@@ -0,0 +1,8 @@
+package org.sopt.seminar_2_android.data.model.local
+
+data class OneReviewData(
+    val image : Int,
+    val name : String,
+    val date : String,
+    val review : String
+)
--- app/src/main/java/org/sopt/seminar_2_android/ui/home/BestBookFragment.kt
+++ app/src/main/java/org/sopt/seminar_2_android/ui/home/BestBookFragment.kt
@@ -6,12 +6,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.sopt.seminar_2_android.R
+import org.sopt.seminar_2_android.data.model.local.OneReviewData
 import org.sopt.seminar_2_android.databinding.FragmentBestBookBinding
 import org.sopt.seminar_2_android.ui.base.BaseFragment
+import org.sopt.seminar_2_android.ui.home.adapter.BestBookRecyclerAdapter
 
 
 class BestBookFragment : BaseFragment<FragmentBestBookBinding>(R.layout.fragment_best_book) {
+    private lateinit var bestBookRecyclerAdapter: BestBookRecyclerAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initBestBook()
+    }
+
+
+
+    private fun initBestBook(){
+        val oneReviewData = mutableListOf(
+            OneReviewData(
+                R.drawable.ic_profile_gray,
+                "아요짱",
+                "2021.11.05",
+                "책보면서 먹는 사과가 젤 맛있쪙!"
+            ),
+            OneReviewData(
+                R.drawable.ic_profile_gray,
+                "안드킹",
+                "2021.11.05",
+                "페니가 성장한 만큼,저도 성장했습니다!"
+            )
+        )
+
+        bestBookRecyclerAdapter = BestBookRecyclerAdapter()
+        binding.rcBestBook.adapter = bestBookRecyclerAdapter
+        bestBookRecyclerAdapter.setReviewData(oneReviewData)
     }
 }

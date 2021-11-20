6b415c16a379fd3a4846bfd56fe904148768935b
hotaek <kht072623@naver.com>
2021-11-20 15:33:32 +0900

    [CHORE] api

--- app/src/main/java/org/sopt/seminar_2_android/data/ApiService.kt
+++ app/src/main/java/org/sopt/seminar_2_android/data/ApiService.kt
@@ -1,4 +1,12 @@
 package org.sopt.seminar_2_android.data
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiService {
+    private const val BASE_URL = ""
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 }
